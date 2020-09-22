48e400e30d460166c6197d4b0b11e3c917795e99
reini <reinhold.schachner@hotmail.com>
2020-09-22 11:34:23 +0200

    Add new books

--- library.js
+++ library.js
@@ -1,3 +1,15 @@
+//in this array the books will be stored
+let library = [];
+
+//variables
+const addBtn = document.querySelector("#addBtn");
+const author = document.querySelector("#author");
+const title = document.querySelector("#title");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
+
+addBtn.addEventListener("click", addBook);
+
 function Book(author, title, pages, read) {
     this.author = author;
     this.title = title;
@@ -8,5 +20,18 @@ function Book(author, title, pages, read) {
     }
   }
 
-  const hobbit = new Book("J.R.R. Tolkien", "The Hobbit", 300, true);
-  console.log(hobbit.info());
+function addBook() {
+  const newBook = new Book(
+    author.value,
+    title.value,
+    pages.value,
+    read.checked
+  );
+  library.push(newBook);
+}
+
+const hobbit = new Book("J.R.R. Tolkien", "The Hobbit", 300, true);
+library.push(hobbit);
+console.log(hobbit.info());
+
+
